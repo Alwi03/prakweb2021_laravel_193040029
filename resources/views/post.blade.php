6bfc9a8a8a094b63a9f3a296433d788e95f8cec3
Muhammad Alwi Ramadhan <alwi.romadhon@gmail.com>
2021-10-14 19:41:27 +0700

    Menyelesaikan Video ke 8 Membuat Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,11 +1,12 @@
-@extends('layouts/main')
+@extends('layouts.main')
 
 @section('container')
-<article>
-    <h1 class="mb-5">{{ $post->title }}</h1>
 
+
+    <h2 class="mb-5">{{ $post->title }}</h2>
+    <p>By. Alwi Ramadhan in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
     {!! $post->body !!}
-</article>
-<a href="/blog">Back to Posts</a>
 
+
+<a href="/blog">Back to Posts</a>
 @endsection
