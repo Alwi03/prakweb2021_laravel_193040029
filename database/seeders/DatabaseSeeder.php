3d7043e8c6e6a8927c32036d63ab965cbf789f01
Muhammad Alwi Ramadhan <alwi.romadhon@gmail.com>
2021-10-21 08:50:18 +0700

    Menyelesaikan Video ke 9 Membuat Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,66 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Alwi Ramadhan',
+            'email' => 'alwi.romadhon@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Yusril Ismail Azi',
+            'email' => 'yusril@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'Web-programming'
+
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est facilis vitae vero blanditiis enim, sint cumque esse nam amet maxime voluptates dolore, rerum obcaecati nesciunt magni provident ab in sequi explicabo? Sequi illum illo, nemo iure, optio eius doloribus nihil architecto quas cumque consequatur quisquam nisi non provident, atque inventore similique dignissimos corporis. Atque, deserunt tempora, explicabo dolorum, laborum illum sit itaque error incidunt odio unde quod! Cum dolores animi, laboriosam culpa, possimus est error veniam doloribus, quaerat magnam repellendus tempora ex suscipit.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est facilis vitae vero blanditiis enim, sint cumque esse nam amet maxime voluptates dolore, rerum obcaecati nesciunt magni provident ab in sequi explicabo? Sequi illum illo, nemo iure, optio eius doloribus nihil architecto quas cumque consequatur quisquam nisi non provident, atque inventore similique dignissimos corporis. Atque, deserunt tempora, explicabo dolorum, laborum illum sit itaque error incidunt odio unde quod! Cum dolores animi, laboriosam culpa, possimus est error veniam doloribus, quaerat magnam repellendus tempora ex suscipit. Doloremque, esse? Laboriosam deleniti, tenetur dolores nulla quas incidunt animi voluptas dolor architecto rerum eveniet, sequi voluptatibus.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est facilis vitae vero blanditiis enim, sint cumque esse nam amet maxime voluptates dolore, rerum obcaecati nesciunt magni provident ab in sequi explicabo? Sequi illum illo, nemo iure, optio eius doloribus nihil architecto quas cumque consequatur quisquam nisi non provident, atque inventore similique dignissimos corporis. Atque, deserunt tempora, explicabo dolorum, laborum illum sit itaque error incidunt odio unde quod! Cum dolores animi, laboriosam culpa, possimus est error veniam doloribus, quaerat magnam repellendus tempora ex suscipit.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est facilis vitae vero blanditiis enim, sint cumque esse nam amet maxime voluptates dolore, rerum obcaecati nesciunt magni provident ab in sequi explicabo? Sequi illum illo, nemo iure, optio eius doloribus nihil architecto quas cumque consequatur quisquam nisi non provident, atque inventore similique dignissimos corporis. Atque, deserunt tempora, explicabo dolorum, laborum illum sit itaque error incidunt odio unde quod! Cum dolores animi, laboriosam culpa, possimus est error veniam doloribus, quaerat magnam repellendus tempora ex suscipit. Doloremque, esse? Laboriosam deleniti, tenetur dolores nulla quas incidunt animi voluptas dolor architecto rerum eveniet, sequi voluptatibus.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est facilis vitae vero blanditiis enim, sint cumque esse nam amet maxime voluptates dolore, rerum obcaecati nesciunt magni provident ab in sequi explicabo? Sequi illum illo, nemo iure, optio eius doloribus nihil architecto quas cumque consequatur quisquam nisi non provident, atque inventore similique dignissimos corporis. Atque, deserunt tempora, explicabo dolorum, laborum illum sit itaque error incidunt odio unde quod! Cum dolores animi, laboriosam culpa, possimus est error veniam doloribus, quaerat magnam repellendus tempora ex suscipit.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est facilis vitae vero blanditiis enim, sint cumque esse nam amet maxime voluptates dolore, rerum obcaecati nesciunt magni provident ab in sequi explicabo? Sequi illum illo, nemo iure, optio eius doloribus nihil architecto quas cumque consequatur quisquam nisi non provident, atque inventore similique dignissimos corporis. Atque, deserunt tempora, explicabo dolorum, laborum illum sit itaque error incidunt odio unde quod! Cum dolores animi, laboriosam culpa, possimus est error veniam doloribus, quaerat magnam repellendus tempora ex suscipit. Doloremque, esse? Laboriosam deleniti, tenetur dolores nulla quas incidunt animi voluptas dolor architecto rerum eveniet, sequi voluptatibus.',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est facilis vitae vero blanditiis enim, sint cumque esse nam amet maxime voluptates dolore, rerum obcaecati nesciunt magni provident ab in sequi explicabo? Sequi illum illo, nemo iure, optio eius doloribus nihil architecto quas cumque consequatur quisquam nisi non provident, atque inventore similique dignissimos corporis. Atque, deserunt tempora, explicabo dolorum, laborum illum sit itaque error incidunt odio unde quod! Cum dolores animi, laboriosam culpa, possimus est error veniam doloribus, quaerat magnam repellendus tempora ex suscipit.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est facilis vitae vero blanditiis enim, sint cumque esse nam amet maxime voluptates dolore, rerum obcaecati nesciunt magni provident ab in sequi explicabo? Sequi illum illo, nemo iure, optio eius doloribus nihil architecto quas cumque consequatur quisquam nisi non provident, atque inventore similique dignissimos corporis. Atque, deserunt tempora, explicabo dolorum, laborum illum sit itaque error incidunt odio unde quod! Cum dolores animi, laboriosam culpa, possimus est error veniam doloribus, quaerat magnam repellendus tempora ex suscipit. Doloremque, esse? Laboriosam deleniti, tenetur dolores nulla quas incidunt animi voluptas dolor architecto rerum eveniet, sequi voluptatibus.',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
