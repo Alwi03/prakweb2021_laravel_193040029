f304d082332b3f8b4a507f5179b7bd5d38e95c53
Muhammad Alwi Ramadhan <alwi.romadhon@gmail.com>
2021-11-24 18:23:34 +0700

    Menyelesaikan Video ke 19 Membuat Validation & Insert Post

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -2,16 +2,20 @@
 @section('container')
 <div class="container">
     <div class="row my-3">
-      <div class="col-lg-8">
-          <h1 class="mb-3">{{ $post->title }}</h1>
-          
-            <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow left"></span> Back To All My Posts</a>
-            <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
-            <a href="" class="btn btn-danger"><span data-feather="x-circle"></span> Delete</a>
-          <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
-        <article class="my-3 fs-5">
-          {!! $post->body !!}
-        </article>
-      </div>
+        <div class="col-lg-8">
+            <h2 class="mb-3">{{ $post->title }}</h2>
+            
+            <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to all my posts</a>
+            <a href="" class="btn btn-warning"><span data-feather="edit"></span></a>
+            <a href="" class="btn btn-danger"><span data-feather="x-circle"></span></a>
+            <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
+            
+            
+            <article class="my-3 fs-5">
+                {!! $post->body !!}
+            </article>
+        </div>
     </div>
-  </div>
+</div>
+
+@endsection
